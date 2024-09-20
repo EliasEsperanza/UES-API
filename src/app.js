ee16e479dfb8fad10686a5c7becdc9f218d3a52c
DieMT10 <diegomorataya03@gmail.com>
2024-09-19 20:40:59 -0600

    Update app.js
    Se agregaron 2 rutas mas a app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import referenciasRoutes from './routes/referencia.routes.js';
 import aulasRoutes from './routes/aulas.routes.js';
 import aulasReferencias from './routes/aula_referencia.routes.js';
+import aulaZonas from './routes/aulas-zonas.routes.js';
+import Zonas from './routes/zona.routes.js';
 
 const corsOptions = {
     origin: '*',
@@ -19,5 +21,7 @@ app.use(express.json());
 app.use(referenciasRoutes);
 app.use(aulasRoutes);
 app.use(aulasReferencias);
+app.use(aulaZonas);
+app.use(Zonas);
 
 export default app;
