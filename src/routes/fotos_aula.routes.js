7816f05efed18acf9864522c6dc9568c5b828c82
Hieloeston321 <GabrielDelaO235@gmail.com>
2024-10-04 11:56:32 -0600

    La misma funciones pero ordenado de orde ascendente

--- src/routes/fotos_aula.routes.js
+++ src/routes/fotos_aula.routes.js
@@ -7,5 +7,6 @@ router.get('/aula_fotos', getFotosAulas);
 router.get('/aula_fotos/:aula_id/:foto_id', getFotosAulasById);
 router.get('/aula_fotos/:aula_id', getFotosByAulaId);
 router.get('/aula_foto/OrdenAula', getFotosOrdenByAulaId);
-    
+router.get('/aula_foto/:aula_id/OrdenAsc');
+
 export default router;
